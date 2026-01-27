7446309e569f80e02ce9d4e74438940ebb5fb319
lijuansuuab <lijuansuuab@users.noreply.github.com>
2026-01-27 14:54:40 +0000

    [ci] deploy site 3b06beff95336180f24abb754a0247f3cbbd166a

---
+++ assets/use-pagetitle-BO-g7-nV.js
@@ -0,0 +1,1 @@
+import{r as t,M as s}from"./index-CC8I0PBJ.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
--- assets/use-pagetitle-mqD21hm-.js
+++
@@ -1,1 +0,0 @@
-import{r as t,M as s}from"./index-Djp9T4e3.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
